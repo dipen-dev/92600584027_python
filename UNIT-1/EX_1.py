9deeb884726e93f396dc37f77798a677331dd6f5
Dipen Patel <pateldipen928@gmail.com>
2026-08-17 22:17:13 +0530

    Delete UNIT-1 directory

--- UNIT-1/EX_1.py
+++
@@ -1,9 +0,0 @@
-# 1. Write a program to demonstrate basic input and output operations in Python. 
-
-name = input("Enter your name: ")
-age = input("Enter your age: ")
-city = input("Enter your city: ")
-
-print("Hello", name)
-print("Your age is", age)
-print("You live in", city)
